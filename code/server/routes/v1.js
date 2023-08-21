a145f4ec36c9d69615d40a5ed3af62d00e8cd77c
coenvlietstra <coenvlietstra@gmail.com>
2023-08-21 22:09:14 +0200

    Commenting improved

--- code/server/routes/v1.js
+++ code/server/routes/v1.js
@@ -1,23 +1,23 @@
 // Import the required module
 const express = require('express');
 const router = express.Router();
-const registerController = require('../controllers/registerController');
-const loginUser = require('../controllers/authController');
-const { createMenuItem } = require('../controllers/createMenuController');
-const { getDishesFromStartDate } = require('../controllers/getMenuController');
-
+const registerController = require('../controllers/registerController'); // Import user registration controller
+const loginUser = require('../controllers/authController'); // Import user authentication controller
+const { createMenuItem } = require('../controllers/createMenuController'); // Import create menu controller
+const { getDishesFromStartDate } = require('../controllers/getMenuController'); // Import get menu controller
 
 // Define the routes for version 1 (v1) API -- START
 
 // Route for user registration
 router.post('/register', registerController.registerUser);
 
-// Route for user authentication route
+// Route for user authentication
 router.post('/auth', loginUser.loginUser);
 
-// Define the route for creating menu items
+// Route for creating menu items
 router.post('/create-menu', createMenuItem);
 
+// Route for getting menu items based on start date
 router.get('/get-menu', getDishesFromStartDate);
 
 // Define the routes for version 1 (v1) API -- END
